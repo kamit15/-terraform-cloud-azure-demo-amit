db5afcfcd47e4213d6bb4ae4dc3edb5064a63f08
Amit Kumar <amit.k15@outlook.com>
2023-04-10 00:25:30 +0530

    V4 Tag Notification Test

--- terraform-manifests/c3-locals.tf
+++ terraform-manifests/c3-locals.tf
@@ -18,6 +18,6 @@ locals {
     Tag2 = "Terraform-Cloud-Demo1-Workspace-Locked"
     Tag3 = "Terraform-Cloud-Demo1-VCS-Demo"
     Tag4 = "Terraform-Cloud-Demo1-Auto-Apply-Test"
-    #Tag5 = "Notifications Testing"
+    Tag5 = "Notifications Testing"
   }
 }
